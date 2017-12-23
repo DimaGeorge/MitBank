c4c7728bde490fdff23815baf0ef566c6ccd6708
Dima George <andreigeorgedima@gmail.com>
2017-12-23 16:11:15 +0200

    un mic schelet de C#

---
+++ MitBank/Kitchen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MitBank
+{
+    public static class Kitchen
+    {
+        public static void tryConnect(string username, string password)
+        {
+            MessageBox.Show("muita");
+        }
+    }
+}
---
+++ MitBank/LoginForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MitBank
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButtonOK_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.username.Text + " " + this.passwd.Text);
+            Kitchen.tryConnect(this.username.Text, this.passwd.Text);
+        }
+    }
+}
